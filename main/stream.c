d385a78b3309e339d24528e3ff50b8ac0cc3b1b4
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2025-02-22 19:18:55 +0100

    feat: stream lichess TV

--- main/stream.c
+++ main/stream.c
@@ -3,6 +3,7 @@
 #include <stddef.h>
 
 #include "stream.h"
+#include "cJSON.h"
 
 static const char *TAG = "cb_http";
 
@@ -10,11 +11,38 @@ static bool NEW_GAME;
 static bool NEW_POSITION;
 static char FEN_BUF[128];
 
+void cb_handle_data_event(char* data, int len) {
+  cJSON* json = cJSON_ParseWithLength(data, len);
+  if (json == NULL) {
+    const char* err_ptr = cJSON_GetErrorPtr();
+    ESP_LOGE(TAG, "Parse of event data failed with error \"%s\", data \"%.*s\"", err_ptr, len, data);
+    return;
+  }
+  const cJSON* type = cJSON_GetObjectItem(json, "t");
+  if (!cJSON_IsString(type) || type->valuestring == NULL) {
+    ESP_LOGE(TAG, "Event data contains faulty type");
+    goto END;
+  }
+  ESP_LOGI(TAG, "Event type %s", type->valuestring);
+  const cJSON* j_data = cJSON_GetObjectItem(json, "d");
+  const cJSON* fen = cJSON_GetObjectItem(j_data, "fen");
+  if (!cJSON_IsString(fen) || type->valuestring == NULL) {
+    ESP_LOGE(TAG, "Event data contains faulty fen");
+    goto END;
+  }
+  strcpy(FEN_BUF, fen->valuestring);
+  NEW_POSITION = true;
+  if (strcmp(type->valuestring, "featured") == 0)
+    NEW_GAME = true;
+END:
+  cJSON_Delete(json);
+}
+
 esp_err_t cb_http_event_handler(esp_http_client_event_t *evt) {
   switch (evt->event_id) {
   case HTTP_EVENT_ON_DATA:
     ESP_LOGI(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
-    printf("%.*s\n", evt->data_len, (char*)evt->data);
+    cb_handle_data_event((char*)evt->data, evt->data_len);
     break;
   case HTTP_EVENT_ERROR:
     ESP_LOGI(TAG, "HTTP_EVENT_ERROR");
