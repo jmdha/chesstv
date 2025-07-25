7dcd5d7999f16f6f2f0e16fcebd8cf46945ba5c3
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2025-07-25 14:01:14 +0200

    refactor: clean remainder of wifi

--- main/wifi.c
+++ main/wifi.c
@@ -7,11 +7,12 @@
 
 #define SSID CONFIG_ESP_WIFI_SSID
 #define PASS CONFIG_ESP_WIFI_PASSWORD
+#define AUTH WIFI_AUTH_WPA2_PSK
 
 const char *TAG = "wifi";
 
 bool CONNECTED;
-EventGroupHandle_t s_wifi_event_group;
+EventGroupHandle_t EVENT_GROUP;
 
 void event_handler(
 	void*,
@@ -34,36 +35,48 @@ void event_handler(
 	}
 }
 
+wifi_init_config_t generate_init_config() {
+	wifi_init_config_t c = WIFI_INIT_CONFIG_DEFAULT();
+	return c;
+}
+
+wifi_config_t generate_config() {
+	wifi_config_t c = {
+		.sta = {
+			.ssid               = SSID,
+			.password           = PASS,
+			.threshold.authmode = AUTH
+		}
+	};
+	return c;
+}
+
 void wifi_init() {
-	CONNECTED = false;
-  	s_wifi_event_group = xEventGroupCreate();
-  	ESP_ERROR_CHECK(esp_netif_init());
-  	ESP_ERROR_CHECK(esp_event_loop_create_default());
-  	esp_netif_create_default_wifi_sta();
+	wifi_init_config_t           config_init;
+	wifi_config_t                config_wifi;
+	esp_event_handler_instance_t event_instance;
 
-  	wifi_init_config_t config_init = WIFI_INIT_CONFIG_DEFAULT();
-  	printf("run esp_wifi_init\n");
-  	ESP_ERROR_CHECK(esp_wifi_init(&config_init));
-  	esp_event_handler_instance_t instance_any_id;
-  	esp_event_handler_instance_t instance_got_ip;
-  	ESP_ERROR_CHECK(esp_event_handler_instance_register(
-  	    WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, &instance_any_id));
-  	ESP_ERROR_CHECK(esp_event_handler_instance_register(
-  	    IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL, &instance_got_ip));
+	CONNECTED   = false;
+  	EVENT_GROUP = xEventGroupCreate();
+	config_init = generate_init_config();
+	config_wifi = generate_config();
 
-  	wifi_config_t config_wifi = {
-  	    .sta = {.ssid = SSID,
-  	            .password = PASS,
-  	            .threshold.authmode = WIFI_AUTH_WPA2_PSK}};
-  	printf("run esp_wifi_set_mode\n");
-  	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-  	printf("run esp_wifi_set_config\n");
-  	ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &config_wifi));
-  	printf("run esp_wifi_start\n");
-  	ESP_ERROR_CHECK(esp_wifi_start());
-  	printf("run esp_wifi_connect\n");
-  	ESP_ERROR_CHECK(esp_wifi_connect());
-  	printf("wifi_init finished\n");
+	esp_netif_init();
+	esp_event_loop_create_default();
+	esp_netif_create_default_wifi_sta();
+	esp_wifi_init(&config_init);
+	esp_wifi_set_mode(WIFI_MODE_STA);
+	esp_wifi_set_config(WIFI_IF_STA, &config_wifi);
+	esp_event_handler_instance_register(
+		WIFI_EVENT,
+		ESP_EVENT_ANY_ID,
+		&event_handler,
+		NULL,
+		&event_instance
+	);
+
+	esp_wifi_start();
+	esp_wifi_connect();
 }
 
 bool wifi_connnnected() {
