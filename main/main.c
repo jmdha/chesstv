39f67b5e2ed995b083113c5215cdd6f9bfcacdc9
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2025-07-25 12:33:45 +0200

    refactor: cleanup start sequence

--- main/main.c
+++ main/main.c
@@ -1,29 +1,24 @@
-#include <esp_log.h>
 #include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
 #include <nvs_flash.h>
-#include <stdio.h>
 
 #include "controller.h"
 #include "wifi.h"
 #include "stream.h"
 
 void init_nvs() {
-  esp_err_t ret = nvs_flash_init();
-  if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
-      ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-    ESP_ERROR_CHECK(nvs_flash_erase());
-    ret = nvs_flash_init();
-  }
-  ESP_ERROR_CHECK(ret);
+	esp_err_t ret = nvs_flash_init();
+	if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
+	    ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		ret = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK(ret);
 }
 
 void app_main(void) {
-  init_nvs();
-  cb_board_init();
-  cb_wifi_init();
-  while (!cb_wifi_connnnected()) {}
-  vTaskDelay(1000 / portTICK_PERIOD_MS);
-  xTaskCreate(cb_stream_task,  "stream", 4096, NULL, 1, NULL);
-  xTaskCreate(cb_board_update, "board", 4096, NULL, 2, NULL);
+	init_nvs();
+	cb_board_init();
+	cb_wifi_init();
+	xTaskCreate(cb_stream_task,  "stream", 4096, NULL, 1, NULL);
+	xTaskCreate(cb_board_update, "board",  4096, NULL, 2, NULL);
 }
